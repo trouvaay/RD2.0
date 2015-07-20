4af5f69c6c3e8647421fd7f0e08fd62c46b6ed5e
Sergey Golovchenko <sergey@lanasoft.net>
2015-07-20 15:47:38 -0400

    updated models based on review

--- raredoor/raredoor/models.py
+++ raredoor/raredoor/models.py
@@ -36,8 +36,9 @@ class TimestampedModel(models.Model):
     class Meta:
         abstract = True
 
+
 class AbstractImageModel(models.Model):
-    """Abstract image model for ProductIMage and RetailerIMage models.
+    """Abstract image model for ProductImage and RetailerImage models.
     Uses Cloudinary for image rendering
     """
     #main image will be primary/1st displayed to user
@@ -48,20 +49,19 @@ class AbstractImageModel(models.Model):
         abstract = True
 
 
+class Merchant(UUIDModel):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, limit_choices_to=Q(profile__is_merchant=True), null=True, blank=True)
+    address = models.ForeignKey('PostalAddress', blank=True, null=True, default=None)
+    contact_info = models.ForeignKey('ContactInfo', blank=True, null=True, default=True)
+    website = models.URLField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
 # ----------------------------------------------------
 # Choices
 # ----------------------------------------------------
-class OrgType(object):
-    """This class maintains a list of all Org. types"""
-    INDIVIDUAL = 'INDIVIDUAL'
-    CORPORATION = 'CORPORATION'
-
-ORG_TYPES = (
-    (OrgType.INDIVIDUAL, 'Individual'),
-    (OrgType.CORPORATION, 'Corporation'),
-)
-
-
 class DiscountType(object):
     """This class maintains a list of all Discount types"""
     RETAILER_SPECIFIC = 'RETAILER_SPECIFIC'
@@ -92,51 +92,43 @@ class PostalAddress(UUIDModel, TimestampedModel):
     city = models.CharField(max_length=20)
     state = models.CharField(max_length=2)  # TODO: needs list of choices here
     zipcd = models.IntegerField()
-    phone = models.CharField(max_length=120)
     lat = models.FloatField(null=True, blank=True)
     lng = models.FloatField(null=True, blank=True)
     neighborhood = models.CharField(max_length=80)  # TODO: needs list of choices here
+
+
+class ContactInfo(UUIDModel, TimestampedModel):
+    phone = PhoneNumberField(blank=True, null=True, default='')
+    sms_number = PhoneNumberField(blank=True, null=True, default='')
+    email = models.EmailField(blank=True, null=True, default='')
 
 
 class Profile(models.Model):
     """This is the place where we add any fields when we need a new user property"""
 
     user = models.OneToOneField(settings.AUTH_USER_MODEL, blank=False, null=False, related_name='profile')
-    phone = PhoneNumberField(blank=True, null=False, default='')
     is_merchant = models.BooleanField(blank=False, null=False, default=False)
     account_balance = models.DecimalField(help_text="Positive value means credit to the user, negative means user ows to us")
-    shipping_address = models.ForeignKey(blank=True, null=True, default=True)
+    shipping_address = models.ForeignKey(PostalAddress, blank=True, null=True, default=True)
+    contact_info = models.ForeignKey(ContactInfo, blank=True, null=True, default=True)
 
 
-class Shipper(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-    phone = PhoneNumberField(max_length=12)
-    email = models.CharField(max_length=255, unique=True)
-
-
-class Retailer(UUIDModel):
+class Retailer(Merchant):
     legal_name = models.CharField(max_length=255)
     short_name = models.CharField(max_length=100)
-    organization_type = models.CharField(max_length=20, choices=ORG_TYPES)
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, limit_choices_to=Q(profile__is_merchant=True), null=True, blank=True)
-    address = models.ForeignKey(PostalAddress, blank=True, null=True, default=None)
-    website = models.URLField(null=True, blank=True)
-    commision_fee = models.DecimalField(blank=False, null=False, max_digits=8, decimal_places=2)
-    transaction_fee = models.DecimalField(blank=False, null=False, max_digits=8, decimal_places=2)
-    order_prefix = models.CharField(max_length=4, unique=True, blank=False, null=False, help_text='This will be pre-pended to all orders from this retailer')
+
 
 class RetailerImage(AbstractImageModel):
     retailer = models.ForeignKey(Retailer, related_name='images')
     is_logo = models.BooleanField(default=False)
 
 
-class Store(models.Model):
+class Store(Merchant):
     retailer = models.ForeignKey(Retailer, related_name='stores')
-    store_num = models.CharField(max_length=10, null=True, blank=True)
-    description = models.TextField(null=True, blank=True)
-    shippers = models.ManyToManyField(Shipper, null=True, blank=True, related_name='stores')
-    is_featured = models.BooleanField(default=False)
     has_returns = models.BooleanField(default=False)
+    commission_fee = models.DecimalField(blank=False, null=False, max_digits=8, decimal_places=2)
+    transaction_fee = models.DecimalField(blank=False, null=False, max_digits=8, decimal_places=2)
+    order_prefix = models.CharField(max_length=4, unique=True, blank=False, null=False, help_text='This will be pre-pended to all orders from this retailer')
 
 
 class Discount(models.Model):
@@ -177,7 +169,6 @@ class Discount(models.Model):
     percent_off_limit = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True, default=None, help_text='Limits the maximum dollar value of the Percent discount off. This is applicable only when percent_off is set')
 
 
-
 def generate_order_number(order):
     """Generates new order number in the following format:
     
@@ -188,36 +179,54 @@ def generate_order_number(order):
     """
     pass
 
-class Order(TimestampedModel):
-    order_number = models.CharField(primary_key=True, max_lendth=20, blank=False, null=False, default=generate_order_number, editable=False)
+
+class BaseOrder(TimestampedModel):
+    """Contains the fields that are same between Order and Offer"""
     user = models.ForeignKey(settings.AUTH_USER_MODEL, blank=False, null=False, related_name='user_orders')
-    address = models.ForeignKey(PostalAddress)
+    shipping_address = models.ForeignKey(PostalAddress)
     subtotal = models.DecimalField(max_digits=8, decimal_places=2, default=0.00, help_text='Total price of all items before discounts, shipping  and taxes')
     taxes = models.DecimalField(max_digits=8, decimal_places=2, blank=None, null=None, default=0.00, help_text="Taxes in dollars")
+    shipping_price = models.DecimalField(max_digits=8, decimal_places=2, blank=None, null=None, default=0.00, help_text="Price of shipping")
     total_transaction_price = models.DecimalField(max_digits=8, decimal_places=2, default=0.00, help_text='Total purchase price the user paid including promotions and taxes')
 
+    class Meta:
+        abstract = True
 
-class OrderItem(TimestampedModel):
-    order = models.ForeignKey(Order, related_name='order_items')
+class BaseOrderItem(TimestampedModel):
+    """Contains the fields that are same between OrderItem, CartItem and Offer"""
+
+    product_variation = models.ForeignKey('ProductVariation', blank=False, null=False, related_name="product_variation_%(class)s")
+    store = models.ForeignKey(Store, blank=False, null=False, related_name="store_%(class)s")
     quantity = models.PositiveIntegerField(blank=False, null=False)
-    product = models.ForeignKey('Product', blank=False, null=False, related_name='product_orders')
-    original_price = models.DecimalField(max_digits=8, decimal_places=2, default=0.00, help_text='Price of the product at the time of purchase')
+    purchase_price = models.DecimalField(max_digits=8, decimal_places=2, default=0.00, help_text='Price of the product before taxes and discounts')
+
+    class Meta:
+        abstract = True
 
 
-class Offer(TimestampedModel):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, blank=False, null=False, related_name='user_orders')
-    product = models.ForeignKey('Product', blank=False, null=False, related_name='product_orders')
-    address = models.ForeignKey(PostalAddress)
-    offer_price = models.DecimalField(max_digits=8, decimal_places=2, default=0.00, help_text='Price offered by user for the product')
-    taxes = models.DecimalField(max_digits=8, decimal_places=2, blank=None, null=None, default=0.00, help_text="Taxes in dollars")
-    total_transaction_price = models.DecimalField(max_digits=8, decimal_places=2, default=0.00, help_text='Total purchase price including taxes')
-    is_captured = models.BooleanField(default=False)
+class Order(BaseOrder):
+    order_number = models.CharField(primary_key=True, max_lendth=20, blank=False, null=False, default=generate_order_number, editable=False)
+
+
+class OrderItem(BaseOrderItem):
+    order = models.ForeignKey(Order, related_name='items')
+
+
+class Offer(BaseOrderItem, BaseOrder):
     is_active = models.BooleanField(default=True)
     expiration_timestamp = models.DateTimeField(blank=False, null=False, help_text='When this offer expires')
 
 
+class Cart(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='cart')
+
+
+class CartItem(BaseOrderItem):
+    cart = models.ForeignKey(Cart, related_name='items')
+
+
 class DiscountRedemption(models.Model):
-    authuser = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='user_redemptions')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='user_redemptions')
     discount = models.ForeignKey(Discount, blank=False, null=False, related_name='discount_redemptions')
     order = models.ForeignKey(Order, blank=False, null=False, related_name='order_discount_redemptions')
     timestamp = models.DateTimeField(blank=False, null=False, help_text='When this was redeemed')
@@ -228,21 +237,8 @@ class DiscountRedemption(models.Model):
 # ----------------------------------------------------
 # Product-related models
 # ----------------------------------------------------
-class Segment(models.Model):
-    select = models.CharField(unique=True, max_length=55, default='new', null=True, blank=True)
-
-
 class Style(models.Model):
     select = models.CharField(unique=True, max_length=55, default='modern', null=True, blank=True)
-
-
-class FurnitureType(models.Model):
-    select = models.CharField(unique=True, max_length=55, default='seating', null=True, blank=True)
-    is_furniture = models.BooleanField(default=True)
-
-
-class ValueTier(models.Model):
-    select = models.CharField(unique=True, max_length=55, default='mid', null=True, blank=True)
 
 
 class Category(models.Model):
@@ -250,34 +246,61 @@ class Category(models.Model):
 
 
 class Subcategory(models.Model):
+    category = models.ForeignKey(Category, blank=False, null=False)
     select = models.CharField(unique=True, max_length=55, default='bar', null=True, blank=True)
-    trial_product = models.BooleanField(default=False)
-    shipping_charge = models.DecimalField(max_digits=8, decimal_places=2, null=True, blank=True, choices=[(5.00, 5.00), (20.00, 20.00), (50.00, 50.00)])
+
+    # features are product properties that are specific for a particular type
+    # e.g. for beds it could be bed size, for tables that could be number of legs
+    # for chairs that would be seat height, etc.
+    features = models.ManyToManyField('Feature', blank=True, null=True, related_name='subcategories')
+
+class PieceType(models.Model):
+    subcategory = models.ForeignKey(Subcategory, blank=False, null=False)
+    select = models.CharField(unique=True, max_length=55, default='', null=True, blank=True)
+    # features are product properties that are specific for a particular type
+    # e.g. for beds it could be bed size, for tables that could be number of legs
+    # for chairs that would be seat height, etc.
+    features = models.ManyToManyField('Feature', blank=True, null=True, related_name='piece_types')
 
 
 class Color(models.Model):
     select = models.CharField(unique=True, max_length=55, default='blue', null=True, blank=True)
 
-
 class Material(models.Model):
     select = models.CharField(unique=True, max_length=55, default='leather', null=True, blank=True)
 
 
-class Product(UUIDModel, TimestampedModel):
+class DeliverySize(models.Model):
+    select = models.CharField(unique=True, max_length=55, default='', null=True, blank=True)
 
-    manufacturer = models.CharField(max_length=100, null=True, blank=True)
-    manufacturer_sku = models.CharField(max_length=100, null=True, blank=True)
-    upc = models.CharField(max_length=12, null=True, blank=True)
 
-    short_name = models.CharField(max_length=100)
-    slug = models.SlugField(unique=True, max_length=255)
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=100, blank=False, null=False, unique=True)
+
+
+class ProductVariationStore(models.Model):
+    product_variation = models.ForeignKey('ProductVariation', blank=False, null=False)
+    store = models.ForeignKey(Store, blank=False, null=False)
     original_price = models.DecimalField(max_digits=8, decimal_places=2, default=0.00)
     current_price = models.DecimalField(max_digits=8, decimal_places=2, default=0.00)
-    description = models.TextField(null=True, blank=True)
-    store = models.ForeignKey(Store)
-    units = models.IntegerField(default=1)
-    url = models.URLField(null=True, blank=True, max_length=255)
     minimum_offer_price = models.DecimalField(max_digits=8, decimal_places=2, null=True, blank=True, default=None)
+    url = models.URLField(null=True, blank=True, max_length=255)
+    is_available = models.BooleanField(blank=False, null=False)
+
+
+class Feature(models.Model):
+    name = models.CharField(max_length=100, blank=False, null=False)
+    value = models.CharField(max_length=100, blank=False, null=False)
+
+
+class ProductVariation(models.Model):
+    variation_name = models.CharField(max_length=100, blank=True, null=True, help_text='What defines this option, e.g. if Product is a "Pillow" variation name can be "Red" or "Leather"')
+    variation_sku = models.CharField(max_length=100, null=True, blank=True)
+    product = models.ForeignKey('Product', blank=False, null=False, related_name='variations')
+    color = models.ManyToManyField(Color, null=True, blank=True)
+    material = models.ManyToManyField(Material, null=True, blank=True)
+    is_default = models.BooleanField(blank=False, null=False, default=False)
+    stores = models.ManyToManyField(Store, through=ProductVariationStore, related_name='store_products')
 
     # shipping properties
     shipping_width = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
@@ -288,24 +311,38 @@ class Product(UUIDModel, TimestampedModel):
     width = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     depth = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     height = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    seat_height = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     diameter = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    bed_size = models.CharField(max_length=50, null=True, blank=True)
+
+
+class ProductVariationImage(AbstractImageModel):
+    product_variation = models.ForeignKey(ProductVariation, related_name='images')
+
+
+class Product(UUIDModel, TimestampedModel):
+
+    manufacturer = models.ForeignKey(Manufacturer, blank=True, null=True)
+    manufacturer_sku = models.CharField(max_length=100, null=True, blank=True)
+    upc = models.CharField(max_length=12, null=True, blank=True)
+
+    short_name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, max_length=255)
+    description = models.TextField(null=True, blank=True)
+
+    msrp = models.DecimalField(max_digits=8, decimal_places=2, default=0.00)
+
     weight = models.DecimalField(max_digits=5, decimal_places=1, null=True, blank=True)
-    color = models.ManyToManyField(Color, null=True, blank=True)
-    color_description = models.CharField(max_length=100, null=True, blank=True)
-    material = models.ManyToManyField(Material, null=True, blank=True)
-    material_description = models.CharField(max_length=255, null=True, blank=True)
     tags = models.TextField(null=True, blank=True)  # list of tag words
     is_custom = models.BooleanField(default=False)
     is_floor_model = models.BooleanField(default=False)
+    is_vintage = models.BooleanField(blank=False, null=False)
+    is_hand_made = models.BooleanField(blank=False, null=False)
 
     # Categorization
-    segment = models.ManyToManyField(Segment, null=True, blank=True)
     style = models.ManyToManyField(Style, null=True, blank=True, verbose_name='style')
-    furnituretype = models.ManyToManyField(FurnitureType, null=True, blank=True)
     category = models.ManyToManyField(Category, null=True, blank=True)
-    subcategory = models.ManyToManyField(Subcategory, null=True, blank=True)  # required for has_trial
+    subcategory = models.ManyToManyField(Subcategory, null=True, blank=True)
+    piece_type = models.ManyToManyField(PieceType, null=True, blank=True)
+    delivery_size = models.ManyToManyField(DeliverySize, null=True, blank=True)
 
     # Availability
     added_date = models.DateTimeField(auto_now_add=True)
@@ -316,10 +353,5 @@ class Product(UUIDModel, TimestampedModel):
     is_featured = models.BooleanField(default=False)
     is_recent = models.BooleanField(default=True)
     hours_left = models.IntegerField(default=settings.SHELF_LIFE, null=True, blank=True)
-    is_landing = models.BooleanField(default=False)
-    lat = models.FloatField(null=True, blank=True)
-    lng = models.FloatField(null=True, blank=True)
-    md5_order = models.CharField(max_length=32, null=True, blank=True)
     click_count = models.IntegerField(blank=False, null=False, default=0)
     display_score = models.IntegerField(blank=False, null=False, default=0)
-    the_hunt = models.BooleanField(default=False)
